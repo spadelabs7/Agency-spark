f831fcecf39575b2c6cd744afb289f240c708aec
Spade Boy <ashiksa88@gmail.com>
2026-06-21 21:42:18 +0530

    Add Netlify adapter

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+import netlify from "@netlify/vite-plugin-tanstack-start"; // <-- Added this
 
 export default defineConfig({
   plugins: [
-    tanstackStart({
-      server: { entry: "server" },
-    }),
+    tanstackStart(),
+    netlify(), // <-- Added this
     viteReact(),
     tailwindcss(),
     tsConfigPaths(),
